fbb7b84c721f6179071683ffa10f2982b49b7212
Raphael Kargon <raphaelkargon@MacBook-Pro-34.local>
2016-06-06 11:17:19 -0400

    formatting fixes

--- examples/bson_streambuf.cpp
+++ examples/bson_streambuf.cpp
@@ -9,92 +9,92 @@
 #include "bson_streambuf.hpp"
 
 bson_output_streambuf::bson_output_streambuf(mongocxx::collection *coll){
-	this->coll = coll;
+    this->coll = coll;
 }
 
 bson_output_streambuf::~bson_output_streambuf(){
-	if (data != nullptr){
-		delete[] data;
-	}
+    if (data != nullptr){
+        delete[] data;
+    }
 }
 
 int bson_output_streambuf::underflow(){
-	return EOF;
+    return EOF;
 }
 
 int bson_output_streambuf::overflow(int ch){
-	int result = EOF;
-	assert(ch >= 0 && ch <= UCHAR_MAX);
-	result = insert(ch);
-	return result;
+    int result = EOF;
+    assert(ch >= 0 && ch <= UCHAR_MAX);
+    result = insert(ch);
+    return result;
 }
 
 int bson_output_streambuf::insert(int ch){
-	bytes_read++;
+    bytes_read++;
 
-	// for first four bytes, build int32 that contains document size
-	if (bytes_read <= 4){
-		len += (ch << (8 * (bytes_read-1)));
-	}
-	// once doc size is received, allocate space.
-	if (bytes_read == 4){
-		// TODO perhaps fail unless 0 < len <= MAX_BSON_SIZE?
-		data = new uint8_t[len];
-		*(size_t *)data = len;
-	}
+    // for first four bytes, build int32 that contains document size
+    if (bytes_read <= 4){
+        len += (ch << (8 * (bytes_read-1)));
+    }
+    // once doc size is received, allocate space.
+    if (bytes_read == 4){
+        // TODO perhaps fail unless 0 < len <= MAX_BSON_SIZE?
+        data = new uint8_t[len];
+        *(size_t *)data = len;
+    }
 
-	if (bytes_read > 4){
-		data[bytes_read-1] = (uint8_t)ch;
-	}
+    if (bytes_read > 4){
+        data[bytes_read-1] = (uint8_t)ch;
+    }
 
-	if (bytes_read == len){
-		// insert document, reset data and length
-		bsoncxx::document::view v(data, len);
-		coll->insert_one(v);
-		delete[] data;
-		data = nullptr;
-		bytes_read = 0;
-		len = 0;
-	} else if (bytes_read > len && len >= 4){
-		return EOF;
-	}
-	return ch;
+    if (bytes_read == len){
+        // insert document, reset data and length
+        bsoncxx::document::view v(data, len);
+        coll->insert_one(v);
+        delete[] data;
+        data = nullptr;
+        bytes_read = 0;
+        len = 0;
+    } else if (bytes_read > len && len >= 4){
+        return EOF;
+    }
+    return ch;
 }
 
 int main(int argc, char const *argv[]) {
-	// init database
+    // init database
     mongocxx::instance inst{};
     mongocxx::client conn{mongocxx::uri{}};
     auto collection = conn["testdb"]["testcollection"];
 
- 	// set up test BSON document
-    std::string json_str = "{\"a\": 1}";
+     // set up test BSON document
+    std::string json_str = "{\"a\": 1, \"b\":[1,2,3], \"c\": {\"a\": 1}}";
     auto bson_obj = bsoncxx::from_json(json_str);
     const uint8_t *data = bson_obj.view().data();
 
-	// set up stream
-	bson_output_streambuf doc_streambuf(&collection);
-	std::ostream doc_stream(&doc_streambuf);
+    // set up stream
+    bson_output_streambuf doc_streambuf(&collection);
+    std::ostream doc_stream(&doc_streambuf);
 
-	// print coll's document count before insert
-	auto cursor = collection.find({});
-	int size = std::distance(cursor.begin(), cursor.end());
-	std::cout << "Before insert: " << size << std::endl;
+    // print coll's document count before insert
+    auto cursor = collection.find({});
+    int size = std::distance(cursor.begin(), cursor.end());
+    std::cout << "Before insert: " << size << std::endl;
 
-	// write document to stream
-	int len = *(int *)data;
-	doc_stream.write((const char *)data, len);
+    // write document to stream
+    int len = *(int *)data;
+    doc_stream.write((const char *)data, len);
 
-	// print coll's document count after insert
-	cursor = collection.find({});
-	size = std::distance(cursor.begin(), cursor.end());
-	std::cout << "After 1st insert: " << size << std::endl;
+    // print coll's document count after insert
+    cursor = collection.find({});
+    size = std::distance(cursor.begin(), cursor.end());
+    std::cout << "After 1st insert: " << size << std::endl;
 
-	doc_stream.write((const char *)data, len);
+    doc_stream.write((const char *)data, len);
 
-	// print coll's document count after insert
-	cursor = collection.find({});
-	size = std::distance(cursor.begin(), cursor.end());
-	std::cout << "After 2nd insert: " << size << std::endl;
- 	return 0;
+    // print coll's document count after insert
+    cursor = collection.find({});
+    size = std::distance(cursor.begin(), cursor.end());
+    std::cout << "After 2nd insert: " << size << std::endl;
+     return 0;
  }
--- examples/bson_streambuf.hpp
+++ examples/bson_streambuf.hpp
@@ -6,54 +6,55 @@
 #include <mongocxx/collection.hpp>
 
 /**
- * A streambuffer that accepts bytes of BSON data,
- * and inserts a document into the given collection when
- * all bytes are sent over. 
- */
+* A streambuffer that accepts bytes of BSON data,
+* and inserts a document into the given collection when
+* all bytes are sent over.
+*/
 class bson_output_streambuf : public std::streambuf
 {
 public:
-	/**
-	 * Constructs a new BSON Output Streambuffer
-	 * that inserts documents into the given collection.
-	 * @param coll A pointer to a MongoDB collection.
-	 */
-	bson_output_streambuf(mongocxx::collection *coll);
+    /**
+    * Constructs a new BSON Output Streambuffer
+    * that inserts documents into the given collection.
+    * @param coll A pointer to a MongoDB collection.
+    */
+    bson_output_streambuf(mongocxx::collection *coll);
 
-	~bson_output_streambuf();
+    ~bson_output_streambuf();
 
-	/**
-	 * This function is called when writing to the stream and the buffer is full.
-	 * Since we don't define a buffer, this is called with every character.
-	 * @param  ch The byte of BSON to insert.
-	 * @return    The inserted byte, or EOF if something failed.
-	 */
-	virtual int overflow(int ch);
+    /**
+    * This function is called when writing to the stream and the buffer is full.
+    * Since we don't define a buffer, this is called with every character.
+    * @param  ch The byte of BSON to insert.
+    * @return    The inserted byte, or EOF if something failed.
+    */
+    virtual int overflow(int ch);
 
-	/**
-	 * This function always returns EOF,
-	 * since one should not write from an output stream.
-	 * @return EOF
-	 */
-	virtual int underflow();
+    /**
+    * This function always returns EOF,
+    * since one should not write from an output stream.
+    * @return EOF
+    */
+    virtual int underflow();
 
 private:
 
-	/**
-	 * Insert a byte of BSON data into the buffer.
-	 * The first four bytes are stored in an int, and determine the document size.
-	 * Then, that number of bytes (minus the first 4) are stored in the buffer.
-	 * When the data is complete a BSON document view is created, and inserted into
-	 * the collection.
-	 *
-	 * @param  ch The byte to insert.
-	 * @return    The byte inserted, or EOF if something failed.
-	 */
-	int insert(int ch);
-	mongocxx::collection *coll;
-	uint8_t *data;
-	size_t len;
-	size_t bytes_read;
+    /**
+    * Insert a byte of BSON data into the buffer.
+    * The first four bytes are stored in an int, and determine the document size.
+    * Then, that number of bytes (minus the first 4) are stored in the buffer.
+    * When the data is complete a BSON document view is created, and inserted into
+    * the collection.
+    *
+    * @param  ch The byte to insert.
+    * @return    The byte inserted, or EOF if something failed.
+    */
+    int insert(int ch);
+    mongocxx::collection *coll;
+    uint8_t *data;
+    size_t len;
+    size_t bytes_read;
+
 };
 
 // _bson_streambuf_hpp
